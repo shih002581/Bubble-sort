6441f308d6283a2727a14c6cc96f9196dba12e74
Administrator <Administrator@A303-7-01>
2025-12-17 11:32:33 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 12, 5, 4, 2};
+            BubbleSort(arr);
+            Console.WriteLine(string.Join(", ", arr));
+        }
+        static void BubbleSort(int[] data)
+        {
+            for (int i = 0; i < data.Length - 1; i++)
+            {
+                for (int j = 0; j < data.Length - 1 - i; j++) 
+                {
+                    if (data[j] > data[j + 1])
+                    {
+                        int temp = data[j];
+                        data[j] = data[j + 1];
+                        data[j+1] = temp;
+                    }
+
+                }
+            }
+        }
+    }
+}
